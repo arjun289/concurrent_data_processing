7b65ca4fa0c14a9461e9ef5b71861cdcaf3bf1f9
arjun289 <arjun289singh@gmail.com>
2021-05-19 22:52:28 +0800

    Add code for airports

--- airports/lib/airports.ex
+++ airports/lib/airports.ex
@@ -1,18 +1,61 @@
 defmodule Airports do
-  @moduledoc """
-  Documentation for Airports.
-  """
+  alias NimbleCSV.RFC4180, as: CSV
 
-  @doc """
-  Hello world.
+  def airports_csv() do
+    Application.app_dir(:airports, "/priv/airports.csv")
+  end
 
-  ## Examples
+  # Using Enum
+  # def open_ariports() do
+  #   airports_csv()
+  #   |> File.read!()
+  #   |> CSV.parse_string()
+  #   |> Enum.map(fn row ->
+  #     %{
+  #       id: Enum.at(row, 0),
+  #       type: Enum.at(row, 2),
+  #       name: Enum.at(row, 3),
+  #       country: Enum.at(row, 8)
+  #     }
+  #   end)
+  #   |> Enum.reject(&(&1.type == "closed"))
+  # end
 
-      iex> Airports.hello()
-      :world
+  # Using Stream
+  # def open_ariports() do
+  #   airports_csv()
+  #   |> File.stream!()
+  #   |> CSV.parse_stream()
+  #   |> Stream.map(fn row ->
+  #     %{
+  #       id: :binary.copy(Enum.at(row, 0)),
+  #       type: :binary.copy(Enum.at(row, 2)),
+  #       name: :binary.copy(Enum.at(row, 3)),
+  #       country: :binary.copy(Enum.at(row, 8))
+  #     }
+  #   end)
+  #   |> Stream.reject(&(&1.type == "closed"))
+  #   |> Enum.to_list()
+  # end
 
-  """
-  def hello do
-    :world
+  # Using Flow
+  def open_ariports() do
+    airports_csv()
+    |> File.stream!()
+    |> Flow.from_enumerable()
+    |> Flow.map(fn row ->
+      [row] = CSV.parse_string(row, skip_headers: false)
+      %{
+        id: :binary.copy(Enum.at(row, 0)),
+        type: :binary.copy(Enum.at(row, 2)),
+        name: :binary.copy(Enum.at(row, 3)),
+        country: :binary.copy(Enum.at(row, 8))
+      }
+    end)
+    |> Flow.reject(&(&1.type == "closed"))
+    |> Flow.partition(key: {:key, :country})
+    |> Flow.group_by(&(&1.country))
+    |> Flow.map(fn {country, data} -> {country, Enum.count(data)} end)
+    |> Enum.to_list()
   end
 end
